06fe1f9326914ffe83bbe4f7bc36a2fd43c7ec5a
Michael Thompson <capnsword@gmail.com>
2018-06-03 17:29:41 -0700

    Fixed typo

--- src/main/java/graphics/Renderer.java
+++ src/main/java/graphics/Renderer.java
@@ -122,10 +122,10 @@ public class Renderer
 	 */
 	public void drawRoundedRectangle(float p_Radius, float p_Width, float p_Height)
 	{
-		VertexArray topleftCorner = constructArk(p_Radius, 5, 180f, 90f);
-		VertexArray toprightCorner = constructArk(p_Radius, 5, 270f, 90f);
-		VertexArray bottomleftCorner = constructArk(p_Radius, 5, 90f, 90f);
-		VertexArray bottomrightCorner = constructArk(p_Radius, 5, 0f, 90f);
+		VertexArray topleftCorner = constructArc(p_Radius, 5, 180f, 90f);
+		VertexArray toprightCorner = constructArc(p_Radius, 5, 270f, 90f);
+		VertexArray bottomleftCorner = constructArc(p_Radius, 5, 90f, 90f);
+		VertexArray bottomrightCorner = constructArc(p_Radius, 5, 0f, 90f);
 		
 		topleftCorner.move(p_Radius, p_Radius);
 		toprightCorner.move(p_Width - p_Radius, p_Radius);
@@ -143,14 +143,14 @@ public class Renderer
 	
 	public void drawCircle(float p_Radius)
 	{
-		VertexArray circle = constructArk(p_Radius, 20, 0f, 360f);
+		VertexArray circle = constructArc(p_Radius, 20, 0f, 360f);
 		draw(circle, DrawTriangleFan);
 	}
 	
 	/*
 	 * Constructs a vertex array that represents a part of a circle.
 	 */
-	private static VertexArray constructArk(float p_Radius, int p_Slices, float p_FromAngle, float p_Angle)
+	private static VertexArray constructArc(float p_Radius, int p_Slices, float p_FromAngle, float p_Angle)
 	{
 		VertexArray va = new VertexArray();
 		for (int i = 0; i < p_Slices + 1; i++)
